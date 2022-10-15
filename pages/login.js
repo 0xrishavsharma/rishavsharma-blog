a42bfc622ae6e24a10d818893bd4edbcd55f258f
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-10-15 23:38:42 +0530

    Second

--- pages/login.js
+++ pages/login.js
@@ -4,7 +4,16 @@ import Navbar from "../components/Navbar"
 import Link from 'next/link';
 import Register from "../pages/register"
 
+
+
+
 const Login = () => {
+    const showHidePassword = () => {
+        if (typeof window != "undefined") {
+            let show = document.querySelectorAll("passwordIcon");
+
+        }
+    }
     return (
         <div className={[styles.container, ""].join(" ")}>
             <div className={[styles.wrapper, ""].join(" ")}>
@@ -17,21 +26,39 @@ const Login = () => {
                         <form action="">
                             <div className={[styles.inputNameWrapper].join(" ")}>
                                 <div className={[styles.inputWrapper, ""].join(" ")}>
-                                    <label htmlFor=""></label>
-                                    <input type="text" placeholder='' />
+                                    <div className={[styles.inputInnerWrapper, ""].join(" ")} id="inputInnerWrapper">
+                                        <label htmlFor="">First Name</label>
+                                        <input type="text" placeholder='' className={[styles.formInput, ""].join(" ")} />
+                                    </div>
                                 </div>
                                 <div className={[styles.inputWrapper, ""].join(" ")}>
-                                    <label htmlFor=""></label>
-                                    <input type="text" placeholder='' />
+                                    <div className={[styles.inputInnerWrapper, ""].join(" ")} id="inputInnerWrapper">
+                                        <label htmlFor="">Last Name</label>
+                                        <input type="text" placeholder='' className={[styles.formInput, ""].join(" ")} />
+                                    </div>
                                 </div>
                             </div>
                             <div className={[styles.inputWrapper, ""].join(" ")}>
-                                <label htmlFor=""></label>
-                                <input type="text" placeholder='' />
+                                <div className={[styles.inputInnerWrapper, ""].join(" ")} id="inputInnerWrapper">
+                                    <label htmlFor="">Email</label>
+                                    <input type="email" placeholder='' className={[styles.formInput, ""].join(" ")} />
+                                </div>
+
                             </div>
                             <div className={[styles.inputWrapper, ""].join(" ")}>
-                                <label htmlFor=""></label>
-                                <input type="text" placeholder='' />
+                                <div className={[styles.inputInnerWrapper, ""].join(" ")} id="inputInnerWrapper">
+                                    <div className={[styles.inputAndIconWrapper, ""].join(" ")}>
+                                        <div className={[styles.labelAndInput, ""].join(" ")}>
+                                            <label htmlFor="">Password</label>
+                                            <input type="password" placeholder='' className={[styles.formInput, ""].join(" ")} />
+                                        </div>
+                                        <div className={[styles.iconWrapper, ""].join(" ")}>
+                                            <i className={[styles.passwordIcon, styles.passwordIcon1, "bx bx-show passwordIcon"].join(" ")} id="passwordIcon1" onClick={showHidePassword}></i>
+                                            <i className={[styles.passwordIcon, styles.passwordIcon2, "bx bx-hide passwordIcon"].join(" ")} id="passwordIcon2" onClick={showHidePassword}></i>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </form>
                     </div>
